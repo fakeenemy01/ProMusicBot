494c1907ba8a9f3f0173c68948621b466459156e
fakeenemy01 <84990081+fakeenemy01@users.noreply.github.com>
2021-07-23 13:17:43 +0530

    Add files via upload

--- userplugins/reply.py
+++ userplugins/reply.py
@@ -1,1 +1,48 @@
+#MIT License
 
+#Copyright (c) 2021 @Professor_Botz
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+
+from pyrogram import Client, filters
+from utils import USERNAME
+from config import Config
+ADMINS=Config.ADMINS
+from pyrogram.errors import BotInlineDisabled
+@Client.on_message(filters.private & ~filters.bot & filters.incoming & ~filters.service & ~filters.me)
+async def reply(client, message): 
+    try:
+        inline = await client.get_inline_bot_results(USERNAME, "ORU_MANDAN_PM_VANNU")
+        await client.send_inline_bot_result(
+            message.chat.id,
+            query_id=inline.query_id,
+            result_id=inline.results[0].id,
+            hide_via=True
+            )
+    except BotInlineDisabled:
+        for admin in ADMINS:
+            try:
+                await client.send_message(chat_id=admin, text=f"Hey,\nIt seems you have disabled Inline Mode for @{USERNAME}\n\nA Nibba is spaming me in PM, enable inline mode for @{USERNAME} from @Botfather to reply him.")
+            except Exception as e:
+                print(e)
+                pass
+
+    except Exception as e:
+        print(e)
+        pass
